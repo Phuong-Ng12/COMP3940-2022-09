0c9a571c1c71bb95e192ca83f4acc7bc30094428
Phuong Nguyen <maiphuong.nguyen97@gmail.com>
2022-09-21 09:57:59 -0700

    Change redirect page after Sign up as a new user. Get user UUID from database and insert it as foreign key in PHOTOS table. Change tables name to USERS and PHOTOS.

--- LoginServlet.java
+++ LoginServlet.java
@@ -3,27 +3,14 @@ import javax.servlet.*;
 import java.io.*;
 
 import java.sql.*;
-import java.util.Date;
 import java.util.UUID;
-import java.text.*;
 import java.nio.*;
 
 public class LoginServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html>\n" + "<head><title>" + "Login" + "</title></head>\n" + "<body>\n"
-                + "<h1 align=\"center\">" + "Login" + "</h1>\n" + "<form action=\"login\" method=\"POST\">\n"
-                + "Username: <input type=\"text\" name=\"user_id\">\n" + "<br />\n"
-                + "Password: <input type=\"password\" name=\"password\" />\n" + "<br />\n"
-                + "<input type=\"submit\" value=\"Sign up\" />\n" + "</form>\n"
-                + "</form>\n" + "<br />\n" + "<br />\n"
-                + "<form action=\"signin\" method=\"POST\">\n"
-                + "Username: <input type=\"text\" name=\"user_id\">\n" + "<br />\n"
-                + "Password: <input type=\"password\" name=\"password\" />\n" + "<br />\n"
-                + "<input type=\"submit\" value=\"logn in\" />\n" + "</form>\n"
-                + "</form>\n" + "</body>\n</html\n");
-
+        RequestDispatcher view = request.getRequestDispatcher("html/index.html");
+        view.forward(request, response);
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -31,12 +18,15 @@ public class LoginServlet extends HttpServlet {
         String title = "Logged in as: ";
         String username = request.getParameter("user_id");
         String password = request.getParameter("password");
+       
+        
         HttpSession session = request.getSession(true);
         String key = "times";
         int num = 0;
        
         session.setAttribute(key, 0);
         session.setAttribute("USER_ID", username);
+        
 
         Connection con = null;
         try {
@@ -48,19 +38,11 @@ public class LoginServlet extends HttpServlet {
         try {
             con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "oracle1");
             PreparedStatement preparedStatement = con
-                    .prepareStatement("INSERT INTO staff (ID,name, password) VALUES (?,?,?)");
+                    .prepareStatement("INSERT INTO users (ID, name, password) VALUES (?,?,?)");
             UUID uuid = UUID.randomUUID();
             preparedStatement.setBytes(1, asBytes(uuid));
             preparedStatement.setString(2, username);
             preparedStatement.setString(3, password);
-            // File file = new File("C:\\duck.jpg");
-            // FileInputStream fin = new FileInputStream(file);
-            // preparedStatement.setBinaryStream(4, fin);
-
-            // java.util.Date date = new java.util.Date();
-            // long t = date.getTime();
-            // java.sql.Date sqlDate = new java.sql.Date(t);
-            // preparedStatement.setDate(5, sqlDate);
             int row = preparedStatement.executeUpdate();
             preparedStatement.close();
             con.close();
@@ -76,7 +58,7 @@ public class LoginServlet extends HttpServlet {
         }
 
         response.setStatus(302);
-        response.sendRedirect("main");
+        response.sendRedirect("signin");
     }
 
     public static byte[] asBytes(UUID uuid) {
