f8cb39a7ea286b0bd7654f2724791daa23321fc8
RichardOhata <richardohata@gmail.com>
2022-09-24 22:08:33 -0700

    Display intended users photos

--- GalleryServlet.java
+++ GalleryServlet.java
@@ -1,7 +1,13 @@
 import java.io.*;
+import java.sql.PreparedStatement;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
+import javax.swing.text.AbstractDocument.LeafElement;
+
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
+import java.sql.*;
 
 public class GalleryServlet extends HttpServlet {
       private int mCount;
@@ -15,13 +21,40 @@ public class GalleryServlet extends HttpServlet {
                   response.setStatus(302);
                   response.sendRedirect("login");
             }
-            ;
+            
 
+            // Shows Gallery.html
             response.setContentType("text/html");
+            // RequestDispatcher view = request.getRequestDispatcher("html/gallery.html");
+            // view.forward(request, response);
             response.setCharacterEncoding("UTF-8");
-            File dir = new File("C:\\tomcat\\webapps\\photogallery\\images");
-            String[] chld = dir.list();
+            File dir = new File("C:\\tomcat\\webapps\\photogallery\\images"); // Remove later
+            String[] chld = dir.list(); //
             HttpSession session = request.getSession(true);
+
+            // Gets all images posted by the signed in user
+            ArrayList<String> photos = new ArrayList<String>();
+            String userUUID = (String) session.getAttribute("userUUID");
+
+            try {
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "oracle1");
+            PreparedStatement preparedStatement = con.prepareStatement("SELECT FILEN FROM PHOTOS WHERE USERID=?");
+            preparedStatement.setString(1, userUUID);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                 photos.add(rs.getString(1));
+            }
+            
+            } catch (SQLException e) {
+                  while (e != null) {
+                        System.out.println("Message: " + e.getMessage());
+                        System.out.println("SQLState: " + e.getSQLState());
+                        System.out.println("ErrorCode: " + e.getErrorCode());
+                        e = e.getNextException();
+                        System.out.println("");
+                  }
+            }
+            String[] photoArray = photos.toArray(new String[0]);
             PrintWriter out = response.getWriter();
             if (request.getParameterMap().containsKey("SS")) {
                   int num = (Integer) session.getAttribute(key);
@@ -42,14 +75,13 @@ public class GalleryServlet extends HttpServlet {
                   session.setAttribute(key, 10000);
             }
             ;
-
             int num1 = (Integer) session.getAttribute(key);
             if (num1 != 10000) {
-                  int d = chld.length;
+                  int d = photoArray.length;
                   int num2 = num1 % d;
-                  String img_src = chld[num2];
+                  String img_src = photoArray[num2];
                   String alt_text = "SOME IMAGE";
-
+                  
                   out.println("<html>");
                   out.println("<meta charset='UTF-8'>");
                   out.println("<body>");
@@ -74,26 +106,27 @@ public class GalleryServlet extends HttpServlet {
                   out.println("<script>");
                   out.println("function Next() {");
                   out.println("const xhttp = new XMLHttpRequest();");
-                  out.println("var k2='key1'");
+                  out.println("var k2='key'");
                   out.println(" xhttp.open('GET','/photogallery/gallery?SS=k2');");
                   out.println("xhttp.send();}");
                   out.println("function Prev() {");
                   out.println("const xhttp = new XMLHttpRequest();");
-                  out.println("var k3='key1'");
+                  out.println("var k3='key'");
                   out.println(" xhttp.open('GET','/photogallery/gallery?FF=k3');");
                   out.println("xhttp.send();}");
                   out.println("function Auto() {");
                   out.println("const xhttp = new XMLHttpRequest();");
-                  out.println("var k4='key1'");
+                  out.println("var k4='key'");
                   out.println(" xhttp.open('GET','/photogallery/gallery?AA=k4');");
                   out.println("xhttp.send();}");
                   out.println("</script>");
                   out.println("</body></html>");
             } else {
-                  int d = chld.length;
+                  int d = photoArray.length;
                   String alt_text = "SOME IMAGE";
                   for (int k = 0; k < d; k++) {
-                        String img_src = chld[k];
+                        String img_src = photoArray[k];
+                        
                         out.println("<html>");
                         out.println("<meta charset='UTF-8'>");
                         out.println("<body>");
@@ -103,13 +136,12 @@ public class GalleryServlet extends HttpServlet {
                         out.println("</div>");
                         out.println("</body>");
                         out.println("</body></html>");
-
+                       
                   }
 
             }
 
             out.close();
-
       }
 
       private boolean isLoggedIn(HttpServletRequest req) {
