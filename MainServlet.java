ff2c0701419363f6d83a90dd4a336d7e657e55a4
Phuong Nguyen <maiphuong.nguyen97@gmail.com>
2022-09-24 22:36:17 -0700

    MainServlet printing from java code.

--- MainServlet.java
+++ MainServlet.java
@@ -1,7 +1,6 @@
 import javax.servlet.http.*;
 import javax.servlet.*;
 import java.io.*;
-import java.util.UUID;
 
 public class MainServlet extends HttpServlet {
   
@@ -11,12 +10,36 @@ public class MainServlet extends HttpServlet {
       HttpSession session = request.getSession(false);
       if (session == null) {
         response.setStatus(302);
-        response.sendRedirect("login");	
+	response.sendRedirect("login");	
       }		
-      String userUUID = (String) session.getAttribute("userUUID");
-      System.out.print("here in main : " + userUUID);
+      String title = "Logged in as: ";
+      title += session.getAttribute("USER_ID");
       response.setContentType("text/html");
-      RequestDispatcher dispatcher = request.getRequestDispatcher("html/main.jsp");
-      dispatcher.forward(request, response);
+      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+      String html = docType + "<html>\n" + "<head><title>" + title + "</title></head>\n"
+						+ "<body bgcolor=\"#f0f0f0\">\n" + "<h1 align=\"center\">" + title + "</h1>\n"+
+						"<div style=\"text-align: center;\">\n" +
+						"<form action=\"upload\" method=\"GET\">\n" + 
+                    	"<input type=\"submit\" value=\"UPLOAD\" />\n" +
+                    	"</form>\n" +
+						"</div>\n" +
+						"<div style=\"text-align: center;\">\n" +
+						"<form action=\"gallery\" method=\"GET\">\n" + 
+                    	"<input type=\"submit\" value=\"GALLERY\" />\n" +
+                    	"</form>\n" +
+						"</div>\n" +
+						"<div style=\"text-align: center;\">\n" +
+						"<form action=\"search\" method=\"GET\">\n" +
+                    	"<input type=\"submit\" value=\"SEARCH\" />\n" +
+                    	"</form>\n" +
+						"</div>\n" +
+						"<div style=\"text-align: center;\">\n" +
+						"<form action=\"logout\" method=\"GET\">\n" + 
+                    	"<input type=\"submit\" value=\"LOGOUT\" />\n" +
+                    	"</form>\n" +
+						"</div>\n" +  "</body></html>";
+
+      PrintWriter out = response.getWriter();
+      out.println(html);
   }
 }
